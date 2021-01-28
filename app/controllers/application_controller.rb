184ed20fbb60047458319e77303545491f2f9340
Tim Flood <tflood9@gmail.com>
2021-01-28 13:49:41 -0500

    commit tree f9d58d5f49918db48ef16cc0f408534846752e46

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+    if current_user
+      render :hello
+    else
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    session[:name]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/secret' => 'secrets#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
